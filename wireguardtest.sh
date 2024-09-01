01dcc24168bc364e40e4e01e605f7b3f91725413
forgenator <forgenator@users.noreply.github.com>
2024-09-01 13:53:15 +0300

    Create wireguardtest.sh

---
+++ wireguardtest.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Configuration
+IP=192.168.0.11
+TIMESTAMP=`date "+%Y-%m-%dT%H:%M:%S"`
+
+Gotify_URL="https://xxx.domain.tld"
+Gotify_Token="xxxxx"
+
+notify()
+{
+        curl -X POST -s \
+                -F "title=${1}" \
+                -F "message=${2}" \
+                -F "priority=5" \
+                "${Gotify_URL}/message?token=${Gotify_Token}"
+
+}
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root"
+   exit 1
+fi
+
+
+if ping -c 1 $IP > /dev/null 2>&1; then
+  echo "$TIMESTAMP Internal network reachable." >> /var/log/wireguardtest.log
+  echo "stil wrong ping"
+else
+  notify "Duckpond Wireguard Down" "Wireguard down, trying to fix it." > /dev/null 2>&1
+  echo "$TIMESTAMP Internal network unreachable, trying to fix it." >> /var/log/wireguardtest.log
+  (
+    wg-quick down wg0 > /dev/null 2>&1
+    wg-quick up wg0 > /dev/null 2>&1
+  )
+  if ping -c 1 $IP > /dev/null 2>&1; then
+    echo "$TIMESTAMP Internal network reachable." >> /var/log/wireguardtest.log
+    notify "Duckpond Wireguard up" "Wireguard up" > /dev/null 2>&1
+  fi
+fi
